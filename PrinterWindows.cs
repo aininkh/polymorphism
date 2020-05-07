e571939dbbd23e34a945353ffd6d4b9961ca5aae
aininkh <aini.khasanah@students.amikom.ac.id>
2020-05-07 14:23:06 +0700

    Membuat class Induk

---
+++ PrinterWindows.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism2595
+{
+    public class PrinterWindows
+    {
+        public string namamerk { get; set; }
+
+        public virtual void show()
+        {
+            Console.WriteLine("Printer Windows display dimension : ");
+        }
+
+        public virtual void print()
+        {
+            Console.WriteLine("Printer Windows printing . . . .");
+
+        }
+    }
+}
