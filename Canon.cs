0b7e3493da724e2495818b68ae949e9ac8be0055
aininkh <aini.khasanah@students.amikom.ac.id>
2020-05-07 14:23:55 +0700

    Membuat class Anak

---
+++ Canon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism2595
+{
+    public class Canon : PrinterWindows
+    {
+        public override void show()
+        {
+            Console.WriteLine("Canon display dimension : 9.5*12");
+        }
+
+        public override void print()
+        {
+            Console.WriteLine("Canon printer printing . . . .");
+        }
+    }
+}
